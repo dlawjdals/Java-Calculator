871781dc89abf589b1345aa1e10a3df2acd56530
dlawjdals <yjm91804640@gmail.com>
2025-10-02 13:22:04 +0900

    AdvancedCalculator 개발 완료

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,12 @@ public class Main {
         calc_add.displayResult("+");
 
 
+        AdvancedCalculator calc_mul = new AdvancedCalculator();
+        calc_mul.setNumbers(10, 2);
+        calc_mul.multiply();
+        calc_mul.displayResult("*");
+
+
 
     }
 }

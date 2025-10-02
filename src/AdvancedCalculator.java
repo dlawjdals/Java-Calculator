f772cc4b67642cbe5c909fc2c24d03dfb93f4056
dlawjdals <yjm91804640@gmail.com>
2025-10-02 13:38:16 +0900

    AdvancedCalculator 주석 추가 완료

--- src/AdvancedCalculator.java
+++ src/AdvancedCalculator.java
@@ -1,11 +1,18 @@
+// 계산기 v2.0 관련 클래스
+// https://github.com/dlawjdals/Java-Calculator/issues/1
+
 public class AdvancedCalculator extends Calculator {
+    // 계산기 v2.0 곱하기 기능 개발 완료
+    //https://github.com/dlawjdals/Java-Calculator/issues/3
     @Override
     public int multiply()
     {
         result = num1 * num2;
         return result;
     }
-
+    // 계산기 v2.0 나누기 기능 개발 완료
+    //https://github.com/dlawjdals/Java-Calculator/issues/4
+    @Override
     public int divide()
     {
         result = num1 / num2;

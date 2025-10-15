f102b76ae4ba009846f1e2ca5feca4cbde022e5f
dlawjdals <yjm91804640@gmail.com>
2025-10-15 14:55:02 +0900

    .

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,14 @@
 public class Main {
     public static void main(String[] args) {
         Calculator calc_add = new Calculator();
+
         calc_add.setNumbers(5, 3);
         calc_add.add();
         calc_add.displayResult("+");
-
 
         AdvancedCalculator calc_mul = new AdvancedCalculator();
         calc_mul.setNumbers(5, 3);
         calc_mul.multiply();
         calc_mul.displayResult("*");
-
-
     }
 }

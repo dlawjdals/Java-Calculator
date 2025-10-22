d3f4bf3dfc6367365378636cb67cfc9ef4252915
dlawjdals <yjm91804640@gmail.com>
2025-10-22 23:39:42 +0900

    나머지　연산　기능　결과　출력　기능　추가　완료

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@
 public class Main {
     public static void main(String[] args) {
         Calculator calc_add = new Calculator();
-
         calc_add.setNumbers(5, 3);
         calc_add.add();
         calc_add.displayResult("+");
@@ -11,6 +10,11 @@ public class Main {
         calc_mul.setNumbers(5, 3);
         calc_mul.multiply();
         calc_mul.displayResult("*");
+
+        ScientificCalculator calc_mod = new ScientificCalculator();
+        calc_mod.setNumbers(5, 3);
+        calc_mod.mod();
+        calc_mod.displayResult("%");
 
     }
 }

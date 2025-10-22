6c080e83c03ac88bbe1b36bad5c076839f2a663c
dlawjdals <yjm91804640@gmail.com>
2025-10-22 17:46:28 +0900

    주석 추가 완료

--- src/ScientificCalculator.java
+++ src/ScientificCalculator.java
@@ -1,5 +1,8 @@
 public class ScientificCalculator extends AdvancedCalculator {
 
+
+    // 계산기 v3.0 곱하기 기능 개발 완료
+    //https://github.com/dlawjdals/Java-Calculator/issues/10
     public int mod() {
         result = 0;
 
